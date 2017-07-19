353af9fac880532deda171ab128f6c09cc778453
addday <addday@gmail.com>
2017-07-19 15:45:56 +0800

    优化性能

--- consumer/config/cardone/groovy/top/cardone/controller/v1/log/OperateLogController.groovy
+++ consumer/config/cardone/groovy/top/cardone/controller/v1/log/OperateLogController.groovy
@@ -1,35 +1,23 @@
-package top.cardone.controller.v1.log;
+package top.cardone.controller.v1.log
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import top.cardone.context.ApplicationContextHolder;
-import top.cardone.context.util.MapUtils;
-import top.cardone.context.util.StringUtils;
-import top.cardone.core.CodeException;
-import top.cardone.core.util.func.Func1;
-import top.cardone.log.service.OperateLogService;
-import top.cardone.web.support.WebSupport;
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest
 
 /**
  * Created by yht
  */
 @Controller("top.cardone.controller.v1.log.OperateLogController")
 @RequestMapping("/v1/log/operateLog")
-public class OperateLogController {
+class OperateLogController {
+    private static
+    final org.apache.logging.log4j.Logger log = org.apache.logging.log4j.LogManager.getLogger(OperateLogController.class)
     /**
      * /c0001.json begin
-     **xx/
-     @RequestMapping("/c0001.json")
+     ** xx/
+     @RequestMapping ( " / c 0 0 0 1 . j s o n " )
      @ResponseBody
       public Object c0001Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
@@ -42,186 +30,167 @@ public class OperateLogController {
 
       if (count > 0) {
       throw new CodeException("operateLogCode already exists", "操作日志编号已经存在");
-      }
-
-      return ApplicationContextHolder.getBean(OperateLogService.class).insertByNotExistsCache(input);
+      }return ApplicationContextHolder.getBean(OperateLogService.class).insertByNotExistsCache(input);
       });
-      }
-      /** /c0001.json end **/
+      }/** /c0001.json end    **/
 
     /**
      * /c0002.json begin
-     **xx/
-     @RequestMapping("/c0002.json")
+     ** xx/
+     @RequestMapping ( " / c 0 0 0 2 . j s o n " )
      @ResponseBody
       public Object c0002Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).insertByNotExistsCache(input));
-      }
-      /** /c0002.json end **/
+      }/** /c0002.json end    **/
 
     /**
      * /c0003.json begin
-     **xx/
-     @RequestMapping("/c0003.json")
+     ** xx/
+     @RequestMapping ( " / c 0 0 0 3 . j s o n " )
      @ResponseBody
       public Object c0003Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).insertByNotExistsCache(input));
-      }
-      /** /c0003.json end **/
+      }/** /c0003.json end    **/
 
     /**
      * /c0004.json begin
-     **xx/
-     @RequestMapping("/c0004.json")
+     ** xx/
+     @RequestMapping ( " / c 0 0 0 4 . j s o n " )
      @ResponseBody
       public Object c0004Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).insertByNotExistsCache(input));
-      }
-      /** /c0004.json end **/
+      }/** /c0004.json end    **/
 
     /**
      * /d0001.json begin
-     **xx/
-     @RequestMapping("/d0001.json")
+     ** xx/
+     @RequestMapping ( " / d 0 0 0 1 . j s o n " )
      @ResponseBody
       public Object d0001Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).updateListCache((List) MapUtils.getObject(input, "operateLogIds")));
-      }
-      /** /d0001.json end **/
+      }/** /d0001.json end    **/
 
     /**
      * /d0002.json begin
-     **xx/
-     @RequestMapping("/d0002.json")
+     ** xx/
+     @RequestMapping ( " / d 0 0 0 2 . j s o n " )
      @ResponseBody
       public Object d0002Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).deleteListCache((List) MapUtils.getObject(input, "operateLogIds")));
-      }
-      /** /d0002.json end **/
+      }/** /d0002.json end    **/
 
     /**
      * /d0003.json begin
-     **xx/
-     @RequestMapping("/d0003.json")
+     ** xx/
+     @RequestMapping ( " / d 0 0 0 3 . j s o n " )
      @ResponseBody
       public Object d0003Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).updateListCache((List) MapUtils.getObject(input, "operateLogIds")));
-      }
-      /** /d0003.json end **/
+      }/** /d0003.json end    **/
 
     /**
      * /d0004.json begin
-     **xx/
-     @RequestMapping("/d0004.json")
+     ** xx/
+     @RequestMapping ( " / d 0 0 0 4 . j s o n " )
      @ResponseBody
       public Object d0004Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).updateListCache((List) MapUtils.getObject(input, "operateLogIds")));
-      }
-      /** /d0004.json end **/
+      }/** /d0004.json end    **/
 
     /**
      * /m0001.json begin
-     **xx/
-     @RequestMapping("/m0001.json")
+     ** xx/
+     @RequestMapping ( " / m 0 0 0 1 . j s o n " )
      @ResponseBody
       public Object m0001Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).findOne(input));
-      }
-      /** /m0001.json end **/
+      }/** /m0001.json end    **/
 
     /**
      * /m0002.json begin
-     **xx/
-     @RequestMapping("/m0002.json")
+     ** xx/
+     @RequestMapping ( " / m 0 0 0 2 . j s o n " )
      @ResponseBody
       public Object m0002Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).findOne(input));
-      }
-      /** /m0002.json end **/
+      }/** /m0002.json end    **/
 
     /**
      * /m0003.json begin
-     **xx/
-     @RequestMapping("/m0003.json")
+     ** xx/
+     @RequestMapping ( " / m 0 0 0 3 . j s o n " )
      @ResponseBody
       public Object m0003Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).findOne(input));
-      }
-      /** /m0003.json end **/
-
+      }/** /m0003.json end    **/
 
     //@Value("${app.root}/upload")
     //private String uploadPath;
 
     /**
      * /m0004.json begin
-     **xx/
-     @RequestMapping("/m0004.json")
+     ** xx/
+     @RequestMapping ( " / m 0 0 0 4 . j s o n " )
      @ResponseBody
       public Object m0004Json(HttpServletRequest request, HttpServletResponse response) throws Exception {
       return ApplicationContextHolder.getBean(WebSupport.class).importExcel(request, response,
       (Func1<Object, List<Map<String, Object>>>) inputList -> ApplicationContextHolder.getBean(OperateLogService.class).insertListByNotExistsCache(Lists.newArrayList(inputList)));
-      }
-      /** /m0004.json end **/
+      }/** /m0004.json end    **/
 
     /**
      * /r0001.json begin
-     **xx/
-     @RequestMapping("/r0001.json")
-     @ResponseBody
-      public Object r0001Json(HttpServletRequest request) {
-      return ApplicationContextHolder.getBean(WebSupport.class).func(request,
-      (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).page(input));
-      }
-      /** /r0001.json end **/
+     **/
+    @RequestMapping("/r0001.json")
+    @ResponseBody
+    Object r0001Json(HttpServletRequest request) {
+        ["test": "test"]
+    }
+    /** /r0001.json end **/
 
     /**
      * /r0002.json begin
-     **xx/
-     @RequestMapping("/r0002.json")
+     ** xx/
+     @RequestMapping ( " / r 0 0 0 2 . j s o n " )
      @ResponseBody
       public Object r0002Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).findListByKeyword(input));
-      }
-      /** /r0002.json end **/
+      }/** /r0002.json end    **/
 
     /**
      * /r0003.json begin
-     **xx/
-     @RequestMapping("/r0003.json")
+     ** xx/
+     @RequestMapping ( " / r 0 0 0 3 . j s o n " )
      @ResponseBody
       public Object r0003Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).page(input));
-      }
-      /** /r0003.json end **/
+      }/** /r0003.json end    **/
 
     /**
      * /r0004.json begin
-     **xx/
-     @RequestMapping("/r0004.json")
+     ** xx/
+     @RequestMapping ( " / r 0 0 0 4 . j s o n " )
      @ResponseBody
       public Object r0004Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).page(input));
-      }
-      /** /r0004.json end **/
+      }/** /r0004.json end    **/
 
     /**
      * /u0001.json begin
-     **xx/
-     @RequestMapping("/u0001.json")
+     ** xx/
+     @RequestMapping ( " / u 0 0 0 1 . j s o n " )
      @ResponseBody
       public Object u0001Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
@@ -239,76 +208,67 @@ public class OperateLogController {
       if (!StringUtils.equals(dbOperateLogId, operateLogId)) {
       throw new CodeException("operateLogCode already exists", "操作日志编号已经存在");
       }
-      }
-
-      return ApplicationContextHolder.getBean(OperateLogService.class).updateCache(input);
+      }return ApplicationContextHolder.getBean(OperateLogService.class).updateCache(input);
       });
-      }
-      /** /u0001.json end **/
+      }/** /u0001.json end    **/
 
     /**
      * /u0002.json begin
-     **xx/
-     @RequestMapping("/u0002.json")
+     ** xx/
+     @RequestMapping ( " / u 0 0 0 2 . j s o n " )
      @ResponseBody
       public Object u0002Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).updateCache(input));
-      }
-      /** /u0002.json end **/
+      }/** /u0002.json end    **/
 
     /**
      * /u0003.json begin
-     **xx/
-     @RequestMapping("/u0003.json")
+     ** xx/
+     @RequestMapping ( " / u 0 0 0 3 . j s o n " )
      @ResponseBody
       public Object u0003Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).updateCache(input));
-      }
-      /** /u0003.json end **/
+      }/** /u0003.json end    **/
 
     /**
      * /u0004.json begin
-     **xx/
-     @RequestMapping("/u0004.json")
+     ** xx/
+     @RequestMapping ( " / u 0 0 0 4 . j s o n " )
      @ResponseBody
       public Object u0004Json(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).updateCache(input));
-      }
-      /** /u0004.json end **/
+      }/** /u0004.json end    **/
 
     /**
      * /addModal.json begin
-     **xx/
-     @RequestMapping("/addModal.json")
+     ** xx/
+     @RequestMapping ( " / a d d M o d a l . j s o n " )
      @ResponseBody
       public Object addModalJson(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).findOneByOperateLogId(input));
-      }
-      /** /addModal.json end **/
+      }/** /addModal.json end    **/
 
     /**
      * /modifyModal.json begin
-     **xx/
-     @RequestMapping("/modifyModal.json")
+     ** xx/
+     @RequestMapping ( " / m o d i f y M o d a l . j s o n " )
      @ResponseBody
       public Object modifyModalJson(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).findOneByOperateLogId(input));
-      }
-      /** /modifyModal.json end **/
+      }/** /modifyModal.json end    **/
 
     /**
      * /index.json begin
-     **xx/
-     @RequestMapping("/index.json")
+     ** xx/
+     @RequestMapping ( " / i n d e x . j s o n " )
      @ResponseBody
       public Object indexJson(HttpServletRequest request) {
       return ApplicationContextHolder.getBean(WebSupport.class).func(request,
       (Func1<Object, Map<String, Object>>) input -> ApplicationContextHolder.getBean(OperateLogService.class).findOneByOperateLogId(input));
-      }
-      /** /index.json end **/
+      }/** /index.json end    **/
 }
